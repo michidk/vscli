edd48ea2b836ae8bff3eca6d47df5b3aceb08c00
Michael Lohr <michael@lohr.dev>
2023-03-04 19:20:19 +0100

    Make log messages more slim

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,10 @@ fn main() -> Result<()> {
 
     let opts = Opts::from_args();
 
-    let log_level = opts.verbosity;
-
-    let _ = env_logger::Builder::from_env(
-        env_logger::Env::default().default_filter_or(log_level.as_str()),
+    env_logger::Builder::from_env(
+        env_logger::Env::default().default_filter_or(opts.verbosity.as_str()),
     )
+    .format(log_format)
     .init();
 
     debug!("Parsed Opts:\n{:#?}", opts);
@@ -32,4 +31,26 @@ fn main() -> Result<()> {
     lc.launch()?;
 
     Ok(())
+}
+
+/// Formats the log messages in a minimalistic way, since we don't have a lot of output.
+fn log_format(buf: &mut env_logger::fmt::Formatter, record: &log::Record) -> std::io::Result<()> {
+    let level = record.level();
+    let level_char = match level {
+        log::Level::Trace => 'T',
+        log::Level::Debug => 'D',
+        log::Level::Info => 'I',
+        log::Level::Warn => 'W',
+        log::Level::Error => 'E',
+    };
+    // color using shell escape codes
+    let colored_level = match level {
+        log::Level::Trace => format!("\x1b[37m{}\x1b[0m", level_char),
+        log::Level::Debug => format!("\x1b[36m{}\x1b[0m", level_char),
+        log::Level::Info => format!("\x1b[32m{}\x1b[0m", level_char),
+        log::Level::Warn => format!("\x1b[33m{}\x1b[0m", level_char),
+        log::Level::Error => format!("\x1b[31m{}\x1b[0m", level_char),
+    };
+
+    write!(buf, "{}: {}", colored_level, record.args())
 }
